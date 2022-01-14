79dbdf2907ba208260889eadc32b7134314539da
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-01-14 20:59:44 +0700

    feat get raspberry shadow data on home page

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
 
+import requests
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template("dashboard/home.html", left_medicine=0, right_medicine=0)
+def home():
+    req = requests.get(
+        "https://api.netpie.io/v2/device/shadow/data",
+        auth=(
+            "3df7ac47-5055-43f0-8c3b-dca2e8fee6bd",
+            "fF2cxnCsWzEHk91Be6jGWj6xsJ5vqYPm",
+        ),
+    )
+    req = req.json()
+    left_medicine = req["data"]["left"]
+    right_medicine = req["data"]["right"]
+    return render_template(
+        "dashboard/home.html",
+        left_medicine=left_medicine,
+        right_medicine=right_medicine,
+    )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
