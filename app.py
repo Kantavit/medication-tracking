4425e71bc4d5056ebd89579b0a63612c626fcd59
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-01-14 18:14:08 +0700

    add base layout templates, home page && css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("dashboard/home.html", left_medicine=0, right_medicine=0)
 
 
 if __name__ == "__main__":
